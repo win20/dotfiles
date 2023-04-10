584ecda52daf5758fb20bb49f65b9eed23976247
Win Barua <win.barua2@gmail.com>
2023-04-10 14:05:51 +0100

    nvim tree to show .env

--- nvim/lua/main-setup/plugins/nvim-tree.lua
+++ nvim/lua/main-setup/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-  return
+	return
 end
 
 vim.g.loaded = 1
@@ -9,14 +9,21 @@ vim.g.loaded_netrwPlugin = 1
 vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 
 nvimtree.setup({
-  renderer = {
-    icons = {
-      glyphs = {
-        folder = {
-          arrow_closed = " ",
-          arrow_open = " ",
-        },
-      },
-    },
-  },
+	renderer = {
+		icons = {
+			glyphs = {
+				folder = {
+					arrow_closed = " ",
+					arrow_open = " ",
+				},
+			},
+		},
+	},
+	filters = {
+		dotfiles = false,
+	},
+	git = {
+		enable = true,
+		ignore = false,
+	},
 })
