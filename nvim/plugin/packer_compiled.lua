da5cc426e76944324caa6468cb1fa76a15741531
win20 <win.barua2@gmail.com>
2023-04-12 22:55:11 +0100

    added tsx support for comment.nvim

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -191,6 +191,11 @@ _G.packer_plugins = {
     path = "/home/wbarua/.local/share/nvim/site/pack/packer/opt/nvim-ts-autotag",
     url = "https://github.com/windwp/nvim-ts-autotag"
   },
+  ["nvim-ts-context-commentstring"] = {
+    loaded = true,
+    path = "/home/wbarua/.local/share/nvim/site/pack/packer/start/nvim-ts-context-commentstring",
+    url = "https://github.com/JoosepAlviste/nvim-ts-context-commentstring"
+  },
   ["nvim-web-devicons"] = {
     loaded = true,
     path = "/home/wbarua/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
