3232e0902512460ff868ee96363d1b454b4ed185
Win Barua <win.barua2@gmail.com>
2023-06-14 19:59:34 +0100

    nvim - add autopairs rule for <>

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,7 +1,12 @@
 return function()
   require('nvim-autopairs').setup {}
+  local npairs = require('nvim-autopairs')
+  local Rule = require('nvim-autopairs.rule')
   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
   local cmp = require('cmp')
+
+  npairs.add_rule(Rule('<', '>'))
+
   cmp.event:on(
     'confirm_done',
     cmp_autopairs.on_confirm_done()
