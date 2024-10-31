f19608650da6b7a0acdbf0e9a046570ef5cd1d5a
Win Barua <win.barua2@gmail.com>
2024-10-31 23:33:33 +0000

    chore(nvim, zed): remove nvim and add zed configs

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.color_scheme = 'GitHub Dark'
+config.font = wezterm.font('JetBrains Mono', { weight = "Bold" })
+config.use_fancy_tab_bar = false
+config.enable_tab_bar = false
+config.window_decorations = "TITLE|RESIZE"
+config.window_padding = { 
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 10,
+}
+config.initial_cols = 200
+config.initial_rows = 60
+
+-- and finally, return the configuration to wezterm
+return config
