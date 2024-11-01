472c5444537631f1c98e5f831b5bb85893ebda9b
Win Barua <win.barua2@gmail.com>
2024-11-01 12:55:46 +0000

    chore: remove unused configs

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -4,3 +4,4 @@ ln -s ~/Developer/repos/dotfiles/.config/wezterm ~/.config/wezterm
 ln -s ~/Developer/repos/dotfiles/.config/skhd ~/.config/skhd
 ln -s ~/Developer/repos/dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/Developer/repos/dotfiles/.zshrc ~/.zshrc
+ln -s ~/Developer/repos/dotfiles/.ideavimrc ~/.ideavimrc
