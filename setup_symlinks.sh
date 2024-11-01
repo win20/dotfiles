89223d9975ff717ad99f41b2f20b1601afea97e3
Win Barua <win.barua2@gmail.com>
2024-11-01 11:18:28 +0000

    feat: add symlink setup script

---
+++ setup_symlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -s ~/Developer/repos/dotfiles/.config/wezterm ~/.config/wezterm
+ln -s ~/Developer/repos/dotfiles/.config/skhd ~/.config/skhd
+ln -s ~/Developer/repos/dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/Developer/repos/dotfiles/.zshrc ~/.zshrc
