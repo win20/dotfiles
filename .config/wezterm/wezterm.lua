c9710c37c4c187c2e4f8ae56bf085c2461dd2d06
Win Barua <win.barua2@gmail.com>
2025-01-25 11:44:24 +0000

    fix(wezterm): jetbrains font not showing properly

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,11 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.color_scheme = 'GitHub Dark'
-config.font = wezterm.font('JetBrains Mono', { weight = "Bold" })
+-- config.font = wezterm.font('JetBrains Mono', { weight = "Bold", harfbuzz_features = {"calt=0", "clig=0", "liga=0"} })
+config.font = wezterm.font {
+	family = "JetBrains Mono",
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+}
 config.use_fancy_tab_bar = false
 config.enable_tab_bar = false
 config.window_decorations = "TITLE|RESIZE"
