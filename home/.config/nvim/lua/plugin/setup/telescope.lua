a68b2afdb1c5020f855b5972f6652e13cf8717ab
Win Barua <wj.barua@gmail.com>
2025-04-24 20:32:35 +0100

    feat(nvim): telescope for git status and branches

--- home/.config/nvim/lua/plugin/setup/telescope.lua
+++ home/.config/nvim/lua/plugin/setup/telescope.lua
@@ -48,11 +48,13 @@ return {
     vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Telescope live grep" })
     vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Telescope buffers" })
     vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Telescope recent files" })
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-    vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-    vim.keymap.set("n", "<leader>fb", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-    vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-    vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Search help" })
+    vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Search keymaps" })
+    vim.keymap.set("n", "<leader>fb", builtin.builtin, { desc = "Search select telescope" })
+    vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Search current word" })
+    vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Search diagnostics" })
+    vim.keymap.set("n", "<leader>fgs", builtin.git_status, { desc = "Search git modified files" })
+    vim.keymap.set("n", "<leader>fgb", builtin.git_branches, { desc = "Search git branches" })
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set("n", "<leader>/", function()
       -- You can pass additional configuration to Telescope to change the theme, layout, etc.
