6a37d4350479cf0ee23a1ddd5550a5e241e44121
Win Barua <wj.barua@gmail.com>
2025-05-06 19:36:24 +0100

    feat(nvim): add option to disable format on save

--- home/.config/nvim/lua/plugin/setup/conform.lua
+++ home/.config/nvim/lua/plugin/setup/conform.lua
@@ -2,13 +2,17 @@ return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
-  keys = { { "<leader>fm", "n" } },
+  keys = { { "<leader>fm", "n" }, { "<leader>ft", "n" } },
 
   config = function()
     local conform = require "conform"
     conform.setup {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
@@ -34,8 +38,36 @@ return {
       },
     }
 
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+
     vim.keymap.set("n", "<leader>fm", function()
       conform.format { async = true, lsp_format = "fallback" }
     end, { desc = "conform: format buffer" })
+
+    vim.keymap.set("n", "<leader>ft", function()
+      if vim.b.disable_autoformat then
+        vim.cmd "FormatEnable"
+        vim.notify "Enabled autoformat for current buffer"
+      else
+        vim.cmd "FormatDisable!"
+        vim.notify "Disabled autoformat for current buffer"
+      end
+    end, { desc = "Toggle autoformat for current buffer" })
   end,
 }
